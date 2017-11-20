f0014a6d414fbaef9a232e2577364dc3b21c5fe9
Dushko Klincharov <klincharovdushko@gmail.com>
2017-11-20 01:37:29 +0100

    Done

--- bayes_classifier.py
+++ bayes_classifier.py
@@ -1,4 +1,7 @@
 import pprint as pp
+import numpy as np
+from scipy.stats import norm
+
 # [diplomiral, uspeh, vid, prosek, nepolozeni]
 
 train_data = [[0,  4.5, 0, 8.31, 1],
@@ -17,27 +20,58 @@ train_data = [[0,  4.5, 0, 8.31, 1],
               [2,  3.2, 1,  8.2, 3],
               [2,    2, 0,  6.2, 3]]
 
-meta_data = [('descrete', [0, 1, 2]),
+meta_data = [('discrete', [0, 1, 2]),
              ('continuous'),
-             ('descrete', [0, 1]),
+             ('discrete', [0, 1]),
              ('continuous'),
-             ('descrete', [0, 1, 2, 3])]
+             ('discrete', [0, 1, 2, 3])]
 
-def descrete_prob_dist(category, parameter):
-    
-    for sample in [x for x in train_data if x[0] == category]:
-        
+def discrete_prob_dist(category, parameter):
+    need_for_laplace = False
+    distribution = {x: 0 for x in meta_data[parameter][1]}
+    category_data = [x for x in train_data if x[0] == category]
+    for sample in category_data:
+        distribution[sample[parameter]] += 1
+    for x in distribution.keys():
+        distribution[x] = distribution[x] * 1.0 / len(category_data)
+        if distribution[x] == 0:
+            need_for_laplace = True
+    if(need_for_laplace):
+        p = 1.0 / len(meta_data[parameter][1])
+        for x in distribution.keys():
+            distribution[x] = round((distribution[x] * len(category_data) + p) * 1.0 / (len(category_data) + 1), 3)
+    return distribution
+
+def continuous_prob_dist(category, parameter):
+    param_data = [x[parameter] for x in train_data if x[0] == category]
+    return {'mean': round(np.mean(param_data),3), 'std': round(np.std(param_data),3)}
 
 classifier = {}
 for i in range(len(meta_data[0][1])):
     category = meta_data[0][1][i]
-    classifier[category] = {}
+    classifier[category] = {'p': sum([1 for x in train_data if x[0] == category]) * 1.0 / len(train_data)}
     for j in range(1, len(meta_data)):
         type_of_param = meta_data[j][0]
-        if type_of_param == 'descrete':
-            classifier[i][j] = {x: 0 for x in meta_data[j][1]}
+        if type_of_param == 'discrete':
+            classifier[i][j] = discrete_prob_dist(category, j)
         else:
-            classifier[i][j] = {'mean': 0, 'std': 0}
+            classifier[i][j] = continuous_prob_dist(category, j)
 
-#pp.pprint(classifier, width=1)
+# pp.pprint(classifier, width=2)
 
+test_data = [[4.7, 0, 9, 1],
+             [  3, 0, 9, 1],
+             [  3, 1, 8, 3]]
+
+for sample in test_data:
+    prob = {0: 1, 1: 1, 2: 1}
+    for cat in meta_data[0][1]:
+        for param in range(1, len(meta_data)):
+            if meta_data[param][0] == 'discrete':
+                prob[cat] *= classifier[cat][param][sample[param-1]]
+            else:
+                prob[cat] *= norm(classifier[cat][param]['mean'], classifier[cat][param]['std']).pdf(sample[param-1])
+    s = sum(prob.values())
+    for key in prob:
+        print('{}: {:.3f}%'.format(key, float(prob[key]/s)*100))
+    print
